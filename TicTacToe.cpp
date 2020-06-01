032f2a6208c47bfd13854ead2daa4bbde0781e03
clackgot <clackgot@gmail.com>
2020-06-01 17:32:01 +0300

    comple class Snake

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,7 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-//#include <curses.h>
+#include <curses.h>
 #include <Windows.h>
 #include <string>
 #include <ctime>
@@ -12,13 +12,13 @@ using namespace std;
 
 int getMaxX()
 {
-    return 20;
+    return getmaxx(stdscr)-1;
 }
 
 
 int getMaxY()
 {
-    return 20;
+    return getmaxy(stdscr)-1;
 }
 
 
@@ -70,13 +70,6 @@ public:
     }
 };
 
-
-
-
-
-//0 - Пусто
-//1 - Крестик
-//2 - Нолик
 
 
 
@@ -136,15 +129,194 @@ public:
 
 
 
+
+class Snake
+{
+private:
+
+    vector<Point> body;
+
+public:
+
+    Snake()
+    {
+        body.push_back(Point(2, 2));
+        body.push_back(Point(3, 2));
+        body.push_back(Point(4, 2));
+        body.push_back(Point(4, 3));
+        body.push_back(Point(5, 3));
+        body.push_back(Point(5, 4));
+        body.push_back(Point(5, 5));
+        body.push_back(Point(6, 5));
+        body.push_back(Point(7, 5));
+    }
+
+
+    vector<Point> getBody()
+    {
+        return body;
+    }
+
+
+
+    bool isValidPoint(Point point)
+    {
+        bool inAreaX = point.getX() >= 0 && point.getX() <= getMaxX();
+        bool inAreaY = point.getY() >= 0 && point.getY() <= getMaxY();
+        bool isEmpty = true;
+
+        for (auto it : body)
+        {
+            if (point.getX() == it.getX() && point.getY() == it.getY())
+            {
+                isEmpty = false;
+            }
+        }
+
+        if (inAreaX && inAreaY && isEmpty)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+
+
+
+    void moveUp()
+    {
+        Point newHead = body[0];
+        newHead.setY(newHead.getY()-1);
+        if (isValidPoint(newHead))
+        {
+            body.erase(body.end() - 1);
+            body.insert(body.begin(), newHead);
+        }
+    }
+
+
+
+    void moveDown()
+    {
+        Point newHead = body[0];
+        newHead.setY(newHead.getY() + 1);
+
+        if (isValidPoint(newHead))
+        {
+            body.erase(body.end() - 1);
+            body.insert(body.begin(), newHead);
+        }
+    }
+
+
+    void moveLeft()
+    {
+        Point newHead = body[0];
+        newHead.setX(newHead.getX() - 1);
+
+        if (isValidPoint(newHead))
+        {
+            body.erase(body.end() - 1);
+            body.insert(body.begin(), newHead);
+        }
+    }
+
+    void moveRight()
+    {
+        Point newHead = body[0];
+        newHead.setX(newHead.getX() + 1);
+        if (isValidPoint(newHead))
+        {
+            body.erase(body.end() - 1);
+            body.insert(body.begin(), newHead);
+        }
+    }
+
+
+
+};
+
+
+
 int main()
 
 {
+    /*
     auto point1 = Point(8,18);
     auto point2 = Point(4, 84);
     point1.print();
     point2.print();
     cout<< Point::getDistance(point1, point2)<<endl;
+    */
 
+    Snake snake = Snake();
+
+
+
+    initscr();
+    keypad(stdscr, TRUE);
+    noecho();
+    curs_set(0);
+
+
+    int ch = - 1;
+
+
+
+
+    while (ch != 'q')
+    {
+        erase();
+        for (auto it : snake.getBody())
+        {
+            mvaddch(it.getY(), it.getX(), 'X');
+        }
+        refresh();
+        ch = getch();
+
+        
+        switch (ch)
+        {
+        case KEY_UP:
+        {
+            snake.moveUp();
+            break;
+        }
+
+        case KEY_DOWN:
+        {
+            snake.moveDown();
+            break;
+        }
+
+        case KEY_LEFT:
+        {
+            snake.moveLeft();
+            break;
+        }
+
+        case KEY_RIGHT:
+        {
+            snake.moveRight();
+            break;
+        }
+
+
+        default:
+            break;
+        }
+    }
+
+
+
+
+
+
+    endwin();
     
     
     
