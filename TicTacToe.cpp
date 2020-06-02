33ec6e2ccac95cb916e477e64aa93350d3b597e9
Axel-github <63206172+Axel-github@users.noreply.github.com>
2020-06-02 15:50:55 +0300

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -70,70 +70,29 @@ public:
     }
 };
 
-
-
-
-
-//0 - Пусто
-//1 - Крестик
-//2 - Нолик
-
-
-
-/*class Game
+class Snake 
 {
 private:
-    bool isRun = false;
+    int size;
+    char tail;
+    char head;
+    vector <Point> body;
+
 public:
-    Game()
-    {
-        initscr();
-        start_color();
-        init_pair(1, COLOR_RED, COLOR_BLACK);
-        init_pair(2, COLOR_BLUE, COLOR_BLACK);
-        init_pair(3, COLOR_GREEN, COLOR_BLACK);
-        init_pair(4, COLOR_YELLOW, COLOR_BLACK);
-        noecho();
-        curs_set(0);
 
-    }
-    ~Game()
-    {
-        endwin();
-    }
-    void run()
-    {
-        isRun = true;
-        while (isRun)
-        {
-            erase();
-            for (int y = 0; y < getmaxy(stdscr); y++)
-            {
-                for (int x = 0; x < getmaxx(stdscr); x++)
-                {
-                    attron(COLOR_PAIR(rand() % 4 + 1));
-                    
-                    mvaddch(y,x,'O');
+  Snake()
+  {
+      body.push_back(Point(1, 1));
+      body.push_back(Point(1, 2));
+      body.push_back(Point(2, 2));
+      body.push_back(Point(2, 3));
+      body.push_back(Point(3, 3));
+  
+      
 
 
-                    refresh();
-                    Sleep(1);
-                }
-
-            }
-            refresh();
-            Sleep(3000);
-        }
-
-
-        printw("Stop\n");
-        getch();
-    }
-
-    
+  }
 };
-*/
-
 
 
 int main()
